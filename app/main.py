4bf36c00f2958417d408f5178b586d43a34745ee
Corentin Turgis <corentin.turgis@stayopen.io>
2025-02-07 00:34:19 +0100

    naming

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ import contextlib
 
 from fastapi import FastAPI
 
-from app.core.db import db, DbUsersModel
+from app.api.routes import auth_router
+from app.core.db import db, User
 
 
 @contextlib.asynccontextmanager
 async def lifespan(app: FastAPI):
     if db.is_closed():
         db.connect()
-    db.create_tables([DbUsersModel])
+    db.create_tables([User])
 
     yield
 
@@ -20,7 +21,7 @@ async def lifespan(app: FastAPI):
 def create_application() -> FastAPI:
     app = FastAPI(title="crypto_api", version="1.0.0", lifespan=lifespan)
     # On inclut les routers
-    # app.include_router(users_router, prefix="/users", tags=["users"])
+    app.include_router(auth_router, prefix="/auth", tags=["auth"])
     db.connect()
     return app
 
